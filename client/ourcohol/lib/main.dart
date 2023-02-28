74d555e747a2ebe8c5f5d88695cb028b002b79db
pdh0927 <https://github.com/pdh980927@naver.com>
2023-03-01 02:19:00 +0900

    complete create entire frame

--- client/ourcohol/lib/main.dart
+++ client/ourcohol/lib/main.dart
@@ -1,29 +1,80 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_remix/flutter_remix.dart';
 
 void main() {
   runApp(MaterialApp(
     title: 'Flutter Demo',
     theme: ThemeData(
-      // This is the theme of your application.
-      //
-      // Try running your application with "flutter run". You'll see the
-      // application has a blue toolbar. Then, without quitting the app, try
-      // changing the primarySwatch below to Colors.green and then invoke
-      // "hot reload" (press "r" in the console where you ran "flutter run",
-      // or simply save your changes to "hot reload" in a Flutter IDE).
-      // Notice that the counter didn't reset back to zero; the application
-      // is not restarted.
       primarySwatch: Colors.blue,
     ),
     home: const MyApp(),
   ));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Index 0: Home',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 1: Business',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 2: School',
+      style: optionStyle,
+    ),
+  ];
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    print('tlf');
+    return Scaffold(
+      body: Center(child: Text(_selectedIndex.toString())),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(
+              FlutterRemix.lightbulb_line,
+            ),
+            label: '팁',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(FlutterRemix.calendar_check_fill),
+            label: '캘린더',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(FlutterRemix.team_line),
+            label: '술자리',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(FlutterRemix.account_circle_line),
+            label: '내정보',
+          )
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Color(0xff131313),
+        unselectedItemColor: Color(0xffCACACA),
+        showSelectedLabels: true,
+        showUnselectedLabels: true,
+        onTap: _onItemTapped,
+      ),
+    );
   }
 }
