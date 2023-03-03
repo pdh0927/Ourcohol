14d9bef4d36eec3174d9fd3d4c91f98e93f7fb54
pdh0927 <https://github.com/pdh980927@naver.com>
2023-03-04 01:20:04 +0900

    modify participants field name

--- server/ourcohol/party/models.py
+++ server/ourcohol/party/models.py
@@ -8,13 +8,14 @@ class Party(models.Model):
     name = models.CharField(max_length =  255)
     place = models.CharField(max_length =  255)
     image = models.ImageField(blank=True,null=True)    # 술자리 사진
-    participations = models.ManyToManyField(User, related_name='participate') # 참여자 id
+    participants = models.ManyToManyField(User, related_name='participate_party') # 참여자 id
     king_user_id = models.ManyToManyField(User, blank=True, related_name='win_party') # 가장 많이 먹은 사람 id
     last_user_id = models.ManyToManyField(User, blank=True, related_name='lose_party') # 가장 적게 먹은 사람 id
     started_at = models.DateTimeField
     created_at = models.DateTimeField(default=timezone.now) # 술자리가 만들어진 시간
     ended_at = models.DateTimeField(null=True) 
+    drank_beer = models.IntegerField(default=0)
+    drank_soju = models.IntegerField(default=0)
     is_active = models.BooleanField(default=False) 
     is_delete = models.BooleanField(default=False)  # for soft delete
-    # 먹은 술 총량
     # 방 code
