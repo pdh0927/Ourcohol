95348fe3732e3336659ecf963ce62dd69f9fbbcb
pdh0927 <https://github.com/pdh980927@naver.com>
2023-03-07 17:10:47 +0900

    add api to calendar

--- server/ourcohol/party/serializers.py
+++ server/ourcohol/party/serializers.py
@@ -3,13 +3,13 @@ from .models import Party, Participant
 from accounts.serializers import UserSerializer, Base64Encoding
 
 class PartyRetrieveSerializer(serializers.ModelSerializer):
-    # image_memory = serializers.SerializerMethodField()
+    image_memory = serializers.SerializerMethodField()
     participants = serializers.StringRelatedField(many=True)
 
     class Meta:
         model = Party
-        fields = ['id','name','place', 'image','participants','drank_beer','drank_soju','is_active', 'created_at']
-        # fields = '__all__'
+        # fields = ['id','name','place', 'image','participants','drank_beer','drank_soju','is_active', 'created_at']
+        fields = '__all__'
     
     def get_image_memory(self, party: Party):
         return Base64Encoding.encoding_image(party)
