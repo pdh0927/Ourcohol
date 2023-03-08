6510009f82b9e230587f08a290643385bf3202b9
pdh0927 <https://github.com/pdh980927@naver.com>
2023-03-08 18:25:25 +0900

    create comment view.py

--- server/ourcohol/comment/views.py
+++ server/ourcohol/comment/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentPostSerializer
+    permission_classes = [IsAuthenticated]
