be2ef88db7cf49c25f509d5fd9368d43267b56bb
pdh0927 <https://github.com/pdh980927@naver.com>
2023-03-10 12:57:13 +0900

    move Login() at first page

---
+++ client/ourcohol/lib/home/home.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_remix/flutter_remix.dart';
+import 'package:ourcohol/home/tabs/calendar_page/calendar.dart';
+import 'package:sizer/sizer.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int _selectedIndex = 0;
+
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text('Index 0: Tips'),
+    Calendar(),
+    Text('Index 2: Party'),
+    Text('Index 3: My'),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(child: Center(child: _widgetOptions[_selectedIndex])),
+      bottomNavigationBar: SizedBox(
+        height: 90,
+        width: 100.w,
+        child: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(
+                FlutterRemix.lightbulb_line,
+              ),
+              label: '팁',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(FlutterRemix.calendar_check_fill),
+              label: '캘린더',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(FlutterRemix.team_line),
+              label: '술자리',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(FlutterRemix.account_circle_line),
+              label: '내정보',
+            )
+          ],
+          currentIndex: _selectedIndex,
+          selectedItemColor: Color(0xff131313),
+          unselectedItemColor: Color(0xffCACACA),
+          showSelectedLabels: true,
+          showUnselectedLabels: true,
+          onTap: _onItemTapped,
+        ),
+      ),
+    );
+  }
+}
--- client/ourcohol/lib/user/login.dart
+++ client/ourcohol/lib/user/login.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:email_validator/email_validator.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
+import 'package:ourcohol/home/home.dart';
 import 'package:ourcohol/style.dart';
 import 'package:sizer/sizer.dart';
 import 'package:ourcohol/provider_ourcohol.dart';
@@ -36,11 +37,10 @@ class _LoginState extends State<Login> {
               body: {'email': email, 'password': password});
 
       if (response.statusCode == 200) {
-        var data =
-            await Map.castFrom(json.decode(utf8.decode(response.bodyBytes)));
+        Map.castFrom(json.decode(utf8.decode(response.bodyBytes)));
 
         print('Login Successfully');
-        return;
+        return response.statusCode;
       } else {
         throw Exception("이메일 또는 비밀번호를 잘못 입력하셨습니다.");
       }
@@ -51,143 +51,149 @@ class _LoginState extends State<Login> {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Container(
-        alignment: Alignment.center,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            Container(
-              width: 45.5.w,
-              height: 6.h,
-              child: Text('OURcohol', style: TextStyle(fontSize: 41)),
-            ),
-            Column(
-              children: [
-                Container(
-                  width: 100.w - 32,
-                  height: (100.w - 32) / 7,
-                  margin: EdgeInsets.only(bottom: 17),
-                  decoration:
-                      BoxDecoration(borderRadius: BorderRadius.circular(5)),
-                  child: TextField(
-                    decoration: InputDecoration(
-                      border: InputBorder.none,
-                      focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(style: BorderStyle.none),
-                          borderRadius: BorderRadius.circular(10),
-                          gapPadding: 0),
-                      enabledBorder: OutlineInputBorder(
-                          borderSide: BorderSide(style: BorderStyle.none),
-                          borderRadius: BorderRadius.circular(5),
-                          gapPadding: 0),
-                      hintText: '이메일을 입력해주세요',
-                      hintStyle: TextStyle(color: Color(0xff686868)),
-                      filled: true,
-                      fillColor: Color(0xffE0E0E0),
-                    ),
-                    autofocus: true,
-                    onChanged: (text) {
-                      setState(() {
-                        inputEmail = text;
-                      });
-
-                      print(inputEmail);
-                    },
-                  ),
-                ),
-                Container(
-                  width: 100.w - 32,
-                  height: (100.w - 32) / 7,
-                  margin: EdgeInsets.only(
-                      bottom:
-                          (inputEmail != '' && inputPassword != '') ? 12 : 17),
-                  child: TextField(
-                    obscureText: visible,
-                    decoration: InputDecoration(
-                      border: InputBorder.none,
-                      enabledBorder: OutlineInputBorder(
-                          borderSide: BorderSide(style: BorderStyle.none),
-                          borderRadius: BorderRadius.circular(10),
-                          gapPadding: 0),
-                      focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(style: BorderStyle.none),
-                          borderRadius: BorderRadius.circular(10),
-                          gapPadding: 0),
-                      hintText: '비밀번호를 입력해주세요',
-                      hintStyle: TextStyle(color: Color(0xff686868)),
-                      filled: true,
-                      fillColor: Color(0xffE0E0E0),
-                      suffixIcon: IconButton(
-                        icon: Icon(
-                          visible ? Icons.visibility : Icons.visibility_off,
-                          color: Color(0xffCACACA),
-                        ),
-                        onPressed: () {
-                          setState(() {
-                            visible = !visible;
-                          });
-                        },
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          alignment: Alignment.center,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Container(
+                width: 45.5.w,
+                height: 6.h,
+                child: Text('OURcohol', style: TextStyle(fontSize: 41)),
+              ),
+              Column(
+                children: [
+                  Container(
+                    width: 100.w - 32,
+                    height: (100.w - 32) / 7,
+                    margin: EdgeInsets.only(bottom: 17),
+                    decoration:
+                        BoxDecoration(borderRadius: BorderRadius.circular(5)),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(style: BorderStyle.none),
+                            borderRadius: BorderRadius.circular(10),
+                            gapPadding: 0),
+                        enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(style: BorderStyle.none),
+                            borderRadius: BorderRadius.circular(5),
+                            gapPadding: 0),
+                        hintText: '이메일을 입력해주세요',
+                        hintStyle: TextStyle(color: Color(0xff686868)),
+                        filled: true,
+                        fillColor: Color(0xffE0E0E0),
                       ),
+                      autofocus: true,
+                      onChanged: (text) {
+                        setState(() {
+                          inputEmail = text;
+                        });
+
+                        print(inputEmail);
+                      },
                     ),
-                    onChanged: (text) {
-                      setState(() {
-                        inputPassword = text;
-                      });
-                    },
                   ),
-                ),
-                (inputEmail != '' && inputPassword != '')
-                    ? MaterialButton(
-                        height: (100.w - 32) / 7 + 10,
-                        padding: EdgeInsets.all(5),
-                        onPressed: () {
-                          login(inputEmail, inputPassword);
-                        },
-                        child: Container(
-                            width: 100.w - 32,
-                            height: (100.w - 32) / 7,
-                            alignment: Alignment.center,
-                            decoration: BoxDecoration(
-                                color: const Color(0xff131313),
-                                borderRadius: BorderRadius.circular(10)),
-                            child: Text("로그인 하기", style: textStyle10)))
-                    : Container(
-                        width: 100.w - 32,
-                        height: (100.w - 32) / 7,
-                        alignment: Alignment.center,
-                        decoration: BoxDecoration(
-                            color: const Color(0xffADADAD),
-                            borderRadius: BorderRadius.circular(10)),
-                        child: Text("로그인 하기", style: textStyle10)),
-                Container(
-                  height: 30,
-                  margin: EdgeInsets.only(
-                      top: (inputEmail != '' && inputPassword != '') ? 0 : 5,
-                      left: 13,
-                      right: 13),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      MaterialButton(
-                          height: 30,
-                          padding: EdgeInsets.all(3),
-                          onPressed: () {},
-                          child: Text("아이디/비밀번호 찾기", style: textStyle11)),
-                      MaterialButton(
-                          minWidth: 53,
-                          height: 30,
-                          padding: EdgeInsets.all(3),
-                          onPressed: () {},
-                          child: SizedBox(
-                              width: 50,
-                              child: Text("회원가입", style: textStyle11))),
-                    ],
+                  Container(
+                    width: 100.w - 32,
+                    height: (100.w - 32) / 7,
+                    margin: EdgeInsets.only(
+                        bottom: (inputEmail != '' && inputPassword != '')
+                            ? 12
+                            : 17),
+                    child: TextField(
+                      obscureText: visible,
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(style: BorderStyle.none),
+                            borderRadius: BorderRadius.circular(10),
+                            gapPadding: 0),
+                        focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(style: BorderStyle.none),
+                            borderRadius: BorderRadius.circular(10),
+                            gapPadding: 0),
+                        hintText: '비밀번호를 입력해주세요',
+                        hintStyle: TextStyle(color: Color(0xff686868)),
+                        filled: true,
+                        fillColor: Color(0xffE0E0E0),
+                        suffixIcon: IconButton(
+                          icon: Icon(
+                            visible ? Icons.visibility : Icons.visibility_off,
+                            color: Color(0xffCACACA),
+                          ),
+                          onPressed: () {
+                            setState(() {
+                              visible = !visible;
+                            });
+                          },
+                        ),
+                      ),
+                      onChanged: (text) {
+                        setState(() {
+                          inputPassword = text;
+                        });
+                      },
+                    ),
                   ),
-                )
-              ],
-            )
-          ],
+                  (inputEmail != '' && inputPassword != '')
+                      ? MaterialButton(
+                          height: (100.w - 32) / 7 + 10,
+                          padding: EdgeInsets.all(5),
+                          onPressed: () async {
+                            if (await login(inputEmail, inputPassword) == 200) {
+                              Navigator.of(context).push(MaterialPageRoute(
+                                  builder: (context) => const Home()));
+                            }
+                          },
+                          child: Container(
+                              width: 100.w - 32,
+                              height: (100.w - 32) / 7,
+                              alignment: Alignment.center,
+                              decoration: BoxDecoration(
+                                  color: const Color(0xff131313),
+                                  borderRadius: BorderRadius.circular(10)),
+                              child: Text("로그인 하기", style: textStyle10)))
+                      : Container(
+                          width: 100.w - 32,
+                          height: (100.w - 32) / 7,
+                          alignment: Alignment.center,
+                          decoration: BoxDecoration(
+                              color: const Color(0xffADADAD),
+                              borderRadius: BorderRadius.circular(10)),
+                          child: Text("로그인 하기", style: textStyle10)),
+                  Container(
+                    height: 30,
+                    margin: EdgeInsets.only(
+                        top: (inputEmail != '' && inputPassword != '') ? 0 : 5,
+                        left: 13,
+                        right: 13),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        MaterialButton(
+                            height: 30,
+                            padding: EdgeInsets.all(3),
+                            onPressed: () {},
+                            child: Text("아이디/비밀번호 찾기", style: textStyle11)),
+                        MaterialButton(
+                            minWidth: 53,
+                            height: 30,
+                            padding: EdgeInsets.all(3),
+                            onPressed: () {},
+                            child: SizedBox(
+                                width: 50,
+                                child: Text("회원가입", style: textStyle11))),
+                      ],
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
