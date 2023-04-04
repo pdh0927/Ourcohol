2e1d21fe8a160eccad09fc4579641dfc12c5e01a
pdh0927 <https://github.com/pdh980927@naver.com>
2023-04-04 09:45:01 +0900

    modify layout size

--- client/ourcohol/lib/home/tabs/my_information_page/my_information.dart
+++ client/ourcohol/lib/home/tabs/my_information_page/my_information.dart
@@ -71,33 +71,53 @@ class _MyInformationState extends State<MyInformation> {
                 Text('내 정보'),
                 Row(
                   children: [
-                    (context.read<UserProvider>().image_memory != null
-                        ? Container(
-                            width: 150 / 852 * 100.h,
-                            height: 150 / 852 * 100.h,
-                            margin: const EdgeInsets.only(bottom: 10),
-                            decoration: BoxDecoration(
-                                shape: BoxShape.circle,
-                                image: DecorationImage(
-                                    image: MemoryImage(base64Decode(context
-                                        .read<UserProvider>()
-                                        .image_memory)),
-                                    fit: BoxFit.fill)))
-                        : Container(
-                            width: (100.w - 50) / 7,
-                            height: (100.w - 50) / 7,
-                            margin: const EdgeInsets.only(bottom: 10),
-                            decoration: BoxDecoration(
-                              color: Color(colorList[
-                                  context.read<PartyProvider>().participants[
-                                          context
-                                              .read<PartyProvider>()
-                                              .myPaticipantIndex]['id'] %
-                                      7]),
-                              borderRadius: BorderRadius.circular(100),
-                            ),
-                            child: Icon(FlutterRemix.user_2_fill,
-                                color: Colors.white, size: 40 / 852 * 100.h)))
+                    Stack(
+                      children: [
+                        (context.read<UserProvider>().image_memory != null
+                            ? Container(
+                                width: 100 / 852 * 100.h,
+                                height: 100 / 852 * 100.h,
+                                margin: const EdgeInsets.only(bottom: 10),
+                                decoration: BoxDecoration(
+                                    shape: BoxShape.circle,
+                                    image: DecorationImage(
+                                        image: MemoryImage(base64Decode(context
+                                            .read<UserProvider>()
+                                            .image_memory)),
+                                        fit: BoxFit.fill)))
+                            : Container(
+                                width: 100 / 852 * 100.h,
+                                height: 100 / 852 * 100.h,
+                                alignment: Alignment.center,
+                                margin: const EdgeInsets.only(bottom: 10),
+                                decoration: BoxDecoration(
+                                  color: Color(colorList[
+                                      context.read<UserProvider>().userId % 7]),
+                                  borderRadius: BorderRadius.circular(100),
+                                ),
+                                child: Icon(FlutterRemix.user_fill,
+                                    color: Colors.white,
+                                    size: 75 / 393 * 100.w))),
+                        Container(
+                          height: 30 / 393 * 100.w,
+                          width: 30 / 393 * 100.w,
+                          margin: EdgeInsets.only(
+                              left: 70 / 393 * 100.w, top: 60 / 393 * 100.w),
+                          decoration: BoxDecoration(
+                              border: Border.all(color: Colors.grey),
+                              shape: BoxShape.circle,
+                              color: const Color(0xffFFFFFF)),
+                          child: IconButton(
+                            padding: EdgeInsets.zero,
+                            onPressed: () async {
+                              await pickImage(ImageSource.gallery);
+                            },
+                            icon: Icon(FlutterRemix.camera_line,
+                                color: Colors.grey, size: 25 / 393 * 100.w),
+                          ),
+                        )
+                      ],
+                    ),
                   ],
                 )
               ],
