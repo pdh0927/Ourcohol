f0a52cf6aebdffa72faa1f54e56a19be2304312b
dh <pdh980927@naver.com>
2023-05-10 23:19:04 +0900

    add sample code refresh access token

--- client/ourcohol/lib/home/tabs/party_page/party.dart
+++ client/ourcohol/lib/home/tabs/party_page/party.dart
@@ -35,74 +35,82 @@ class _PartyState extends State<Party> {
   Future getRecentParty() async {
     var party = {};
     http.Response response;
-    if (Platform.isIOS) {
-      response = await http.get(
-          Uri.parse(
-              'http://127.0.0.1:8000/api/participant/recent/${context.read<UserProvider>().userId}/'),
-          headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json',
-            'Authorization':
-                'Bearer ${context.read<UserProvider>().tokenAccess}',
-          });
-
-      if (json.decode(utf8.decode(response.bodyBytes)).length > 0) {
-        party =
-            json.decode(utf8.decode(response.bodyBytes)).toList()[0]['party'];
-
-        context.read<PartyProvider>().setPartyInformation(
-            party['id'],
-            party['image_memory'],
-            party['participants'],
-            party['comments'],
-            party['name'],
-            party['place'],
-            party['image'],
-            party['created_at'],
-            party['started_at'],
-            party['ended_at'],
-            party['drank_beer'],
-            party['drank_soju'],
-            context.read<UserProvider>().userId);
+    try {
+      if (Platform.isIOS) {
+        response = await http.get(
+            Uri.parse(
+                'http://127.0.0.1:8000/api/participant/recent/${context.read<UserProvider>().userId}/'),
+            headers: {
+              'Content-Type': 'application/json',
+              'Accept': 'application/json',
+              'Authorization':
+                  'Bearer ${context.read<UserProvider>().tokenAccess}',
+            });
       } else {
-        context.read<PartyProvider>().initPartyInformation();
+        response = await http.get(
+            Uri.parse(
+                'http://10.0.2.2:8000/api/participant/recent/${context.read<UserProvider>().userId}/'),
+            headers: {
+              'Content-Type': 'application/json',
+              'Accept': 'application/json',
+              'Authorization':
+                  'Bearer ${context.read<UserProvider>().tokenAccess}',
+            });
       }
 
-      return party;
-    } else {
-      response = await http.get(
-          Uri.parse(
-              'http://10.0.2.2:8000/api/participant/recent/${context.read<UserProvider>().userId}/'),
-          headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json',
-            'Authorization':
-                'Bearer ${context.read<UserProvider>().tokenAccess}',
-          });
-      if (json.decode(utf8.decode(response.bodyBytes)).length > 0) {
-        party =
-            json.decode(utf8.decode(response.bodyBytes)).toList()[0]['party'];
+      if (response.statusCode == 200) {
+        if (json.decode(utf8.decode(response.bodyBytes)).length > 0) {
+          party =
+              json.decode(utf8.decode(response.bodyBytes)).toList()[0]['party'];
 
-        context.read<PartyProvider>().setPartyInformation(
-            party['id'],
-            party['image_memory'],
-            party['participants'],
-            party['comments'],
-            party['name'],
-            party['place'],
-            party['image'],
-            party['created_at'],
-            party['started_at'],
-            party['ended_at'],
-            party['drank_beer'],
-            party['drank_soju'],
-            context.read<UserProvider>().userId);
+          context.read<PartyProvider>().setPartyInformation(
+              party['id'],
+              party['image_memory'],
+              party['participants'],
+              party['comments'],
+              party['name'],
+              party['place'],
+              party['image'],
+              party['created_at'],
+              party['started_at'],
+              party['ended_at'],
+              party['drank_beer'],
+              party['drank_soju'],
+              context.read<UserProvider>().userId);
+          print('update success');
+
+          return party;
+        } else {
+          context.read<PartyProvider>().initPartyInformation();
+        }
+        return null;
+      } else if (response.statusCode == 401) {
+        print('token expired');
+        // 여기에 새로 getrecentparty api 호출해야하는데
+        // 그보다 큰 getrecentparty 함수를 만들고 그 안에서 실제 getrecentparty 호출해서 만료됐으면 refresh하고 다시 getrecentparty 호출하는 방법이 좋을듯
+        return null;
       } else {
-        context.read<PartyProvider>().initPartyInformation();
+        print('fail to get');
       }
-
-      return party;
+    } catch (e) {
+      print(e.toString());
     }
+  }
+
+  tokenRefresh() async {
+    http.Response response;
+    if (Platform.isIOS) {
+      // print(context.read<UserProvider>().tokenRefresh);
+      response = await post(
+          Uri.parse(
+              'http://127.0.0.1:8000/api/accounts/jwt-token-auth/refresh/'),
+          body: {'refresh': context.read<UserProvider>().tokenRefresh});
+    } else {
+      response = await http.get(Uri.parse(
+          'http://10.0.2.2:8000/api/participant/recent/${context.read<UserProvider>().userId}/'));
+    }
+    context.read<UserProvider>().tokenAccess =
+        Map.castFrom(json.decode(utf8.decode(response.bodyBytes)))['access'];
   }
 
   updateParty(String key, String value) async {
