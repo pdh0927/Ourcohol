89361ed00a420266778093a809e75c4c42efc85b
dh <pdh980927@naver.com>
2023-05-10 23:19:04 +0900

    add sample code refresh access token

--- server/ourcohol/ourcohol/urls.py
+++ server/ourcohol/ourcohol/urls.py
@@ -3,10 +3,9 @@ from django.urls import path, include
 from ourcohol import views
 
 urlpatterns = [
-    path("", views.home),
+    path('', views.home, name='home'),
     path("admin/", admin.site.urls),
     path("api/accounts/", include("accounts.urls")),
     path("api/party/", include("party.urls")),
     path("api/participant/", include("participant.urls")),
-    path('', views.home, name='home'),
 ]
