11f4423dac9fd59b077aebb52d13b5784450ca06
dh <pdh980927@naver.com>
2023-05-12 23:38:34 +0900

    add / url

--- server/ourcohol/ourcohol/urls.py
+++ server/ourcohol/ourcohol/urls.py
@@ -3,7 +3,9 @@ from django.urls import path, include
 from ourcohol import views
 
 urlpatterns = [
+
     path('', views.home, name='home'),
+
     path("admin/", admin.site.urls),
     path("api/accounts/", include("accounts.urls")),
     path("api/party/", include("party.urls")),
