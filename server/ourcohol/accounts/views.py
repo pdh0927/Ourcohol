5f73b60ac7cd0cc1ef6fa1ab175e1f5096d36965
dh <pdh980927@naver.com>
2023-05-13 13:18:42 +0900

    delete trash annotation

--- server/ourcohol/accounts/views.py
+++ server/ourcohol/accounts/views.py
@@ -18,69 +18,12 @@ class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
     permission_classes = [AllowAny]
 
-    # def create(self, request):
-    #     serializer = UserSerializer(data=request.data)
-    #     if serializer.is_valid():
-    #         user = serializer.save()
-    #         # jwt token 접근해주기
-    #         token = TokenObtainPairSerializer.get_token(user)
-    #         refresh_token = str(token)
-    #         access_token = str(token.access_token)
-    #         res = Response(
-    #             {
-    #                 "user": serializer.data,
-    #                 "message": "register successs",
-    #                 "token": {
-    #                     "access_token": access_token,
-    #                     "refresh_token": refresh_token,
-    #                 },
-    #             },
-    #             status=status.HTTP_200_OK,
-    #         )
-    #         res.set_cookie("access", access_token, httponly=True)
-    #         res.set_cookie("refresh", refresh_token, httponly=True)
-    #         return res
-    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    # @action(methods=['POST'], detail=False)
-    # def login(self, request):
-    #     user = authenticate(
-    #         email=request.data.get("email"), password=request.data.get("password")
-    #     )
-    #     if user is not None:
-    #         serializer = UserSerializer(user)
-    #         token = TokenObtainPairSerializer.get_token(user)
-    #         refresh_token = str(token)
-    #         access_token = str(token.access_token)
-    #         res = Response(
-    #             {
-    #                 "user": serializer.data,
-    #                 "message": "login success",
-    #                 "token": {
-    #                     "access": access_token,
-    #                     "refresh": refresh_token,
-    #                 },
-    #             },
-    #             status=status.HTTP_200_OK,
-    #         )
-    #         return res
-    #     else:
-    #         return Response(status=status.HTTP_400_BAD_REQUEST)
-
-    # def get_permissions(self):
-    #     if self.action == 'login' or self.action == 'create':
-    #         return [AllowAny() ]
-    #     return super(UserViewSet, self).get_permissions()
-
-
 class ConfirmEmailView(APIView):
     permission_classes = [AllowAny]
 
     def get(self, *args, **kwargs):
         self.object = confirmation = self.get_object()
         confirmation.confirm(self.request)
-        # A React Router Route will handle the failure scenario
-        # return HttpResponseRedirect('/') # 인증성공
         return render(self.request, "account/email/email_verity_success.html")
 
     def get_object(self, queryset=None):
