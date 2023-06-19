aba1ce5f1539d4d865f19551486523fe1f3e3e62
dh <pdh980927@naver.com>
2023-06-19 21:35:08 +0900

    add temp page

---
+++ client/ourcohol/lib/home/tabs/tips_page/hangover.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Hangover extends StatelessWidget {
+  const Hangover({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Center(child: const Text('숙취해소 페이지')));
+  }
+}
---
+++ client/ourcohol/lib/home/tabs/tips_page/talk_topics.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TalkTopics extends StatelessWidget {
+  const TalkTopics({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Center(child: const Text('토크 주제 페이지')));
+  }
+}
