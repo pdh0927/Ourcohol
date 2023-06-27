37c15bc33bf158f88a22aacb6634ae5666aa01fc
dh <pdh980927@naver.com>
2023-06-27 14:56:14 +0900

    remove image_memory

--- client/ourcohol/lib/provider_ourcohol.dart
+++ client/ourcohol/lib/provider_ourcohol.dart
@@ -3,7 +3,7 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 class UserProvider extends ChangeNotifier {
   int userId = -1;
-  String image_memory = '';
+  String image = '';
   String email = '';
   String tokenAccess = '';
   String tokenRefresh = '';
@@ -13,12 +13,12 @@ class UserProvider extends ChangeNotifier {
 
   final storage = FlutterSecureStorage(); // FlutterSecureStorage를 storage로 저장
 
-  setUserInformation(userId, email, nickname, image_memory, type_alcohol,
+  setUserInformation(userId, email, nickname, image, type_alcohol,
       amount_alcohol, tokenAccess, tokenRefresh) {
     this.userId = userId;
     this.email = email;
     this.nickname = nickname;
-    this.image_memory = image_memory;
+    this.image = image;
     this.type_alcohol = type_alcohol;
     this.amount_alcohol = amount_alcohol;
     this.tokenAccess = tokenAccess;
@@ -29,7 +29,7 @@ class UserProvider extends ChangeNotifier {
 
   initUserInformation() {
     userId = -1;
-    image_memory = '';
+    image = '';
     email = '';
     tokenAccess = '';
     tokenRefresh = '';
@@ -43,7 +43,6 @@ class UserProvider extends ChangeNotifier {
 
 class PartyProvider extends ChangeNotifier {
   int partyId = -1;
-  String image_memory = '';
   List participants = [];
   List comments = [];
   String name = '';
@@ -57,27 +56,14 @@ class PartyProvider extends ChangeNotifier {
 
   var myPaticipantIndex = -1;
 
-  setPartyInformation(
-      partyId,
-      image_memory,
-      participants,
-      comments,
-      name,
-      place,
-      image,
-      created_at,
-      started_at,
-      ended_at,
-      drank_beer,
-      drank_soju,
-      userId) {
+  setPartyInformation(partyId, image, participants, comments, name, place,
+      created_at, started_at, ended_at, drank_beer, drank_soju, userId) {
     this.partyId = partyId;
-    this.image_memory = image_memory;
+    this.image = image;
     this.participants = participants;
     this.comments = comments;
     this.place = place;
     this.name = name;
-    this.image = image;
     this.created_at = created_at;
     this.started_at = started_at;
     this.ended_at = ended_at;
@@ -102,7 +88,6 @@ class PartyProvider extends ChangeNotifier {
 
   initPartyInformation() {
     partyId = -1;
-    image_memory = '';
     participants = [];
     comments = [];
     name = '';
